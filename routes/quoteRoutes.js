77fb4e7d75d257d4d23b383a2afd4bd0c4421064
Menez <meneznicole117477@gmail.com>
2025-02-22 15:13:04 +0800

    added new route

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -4,6 +4,7 @@ import { createQuote, fetchQuotes } from '../controller/quoteController.js';
 const router = express.Router();
 
 router.post('/createQuote', createQuote);
+router.post('/deleteQuote', createQuote);
 router.get('/', fetchQuotes);
 // router.get('/', (req, res) => {
 //     res.send({Message: "Hello From Quote Route" })
